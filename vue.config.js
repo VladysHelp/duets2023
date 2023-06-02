bbe65586c16d4bafdd5d482c604a66ed0c8b2c95
VKH <vladyshelp>
2023-06-02 18:22:24 +0300

    init

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/choir/",
+  publicPath: "/",
   outputDir: path.resolve(__dirname, "./docs"),
   pwa: {
     workboxOptions: {
